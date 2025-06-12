aaa6e16539f61285ee4e0d9e9ca6206dbc3ab0ab
jeffwoolums <jeffwoolums@mac.com>
2025-06-11 20:03:05 -0500

    Update LessonRequestSchema.js

--- Models/LessonRequestSchema.js
+++ Models/LessonRequestSchema.js
@@ -1,1 +1,25 @@
-
+# Input schema
+const LessonRequestSchema = {
+  lessonSource: {
+    sourceType: "ComeFollowMe | GeneralConference | CustomTopic | LibrarySource",
+    sourceDetails: {
+      year: 2025,
+      weekNumber: 24,
+      lessonLink: "...",
+      talkTitle: "...",
+      talkLink: "...",
+      customPrompt: "...",
+      librarySection: "...",
+      libraryLink: "..."
+    }
+  },
+  settings: {
+    maxParableSlides: 5,
+    maxPointsPerSlide: 5,
+    maxScripturesPerSlide: 4,
+    maxQuotesPerSlide: 3,
+    maxArtworksPerSlide: 1,
+    maxHymnsPerSlide: 1,
+    maxQuestionsPerSlide: 5
+  }
+};
