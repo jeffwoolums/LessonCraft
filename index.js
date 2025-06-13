fd016f1c0fe7eb3358c86f593880d7898aa8dbcd
jeffwoolums <jeffwoolums@mac.com>
2025-06-13 11:54:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const buildPrompt = require('./PromptBuilder');
+const buildPrompt = require('./Prompts/PromptBuilder');
 require('dotenv').config();
 
 const app = express();
