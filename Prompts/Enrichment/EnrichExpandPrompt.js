b298eb18b6f73789a5988af2fb5d84dc8cd6ab93
jeffwoolums <jeffwoolums@mac.com>
2025-06-15 07:52:40 -0500

    Update EnrichExpandPrompt.js

--- Prompts/Enrichment/EnrichExpandPrompt.js
+++ Prompts/Enrichment/EnrichExpandPrompt.js
@@ -1,5 +1,5 @@
 function buildEnrichExpandPrompt(request) {
     // Provide enrichment-specific prompt logic here.
     return "Enrichment prompt (buildEnrichExpandPrompt) with request: " + JSON.stringify(request);
-}
+} 
 module.exports = { buildEnrichExpandPrompt };
