ceea44d5d332ee8cf8432b7885f84b894689cb4c
jeffwoolums <jeffwoolums@mac.com>
2025-06-15 07:47:33 -0500

    Update TalkPrompt.js

--- Prompts/TalkPrompt.js
+++ Prompts/TalkPrompt.js
@@ -2,4 +2,4 @@ function buildTalkPrompt(request) {
     // Example implementation
     return "Generate a talkprompt prompt based on this request: " + JSON.stringify(request);
 }
-module.exports = { buildTalkPrompt };
+module.exports = { buildTalkPrompt }; 
