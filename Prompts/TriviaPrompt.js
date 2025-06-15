2757d855db0bdc43daebf0c764daa17a62dc0ae4
jeffwoolums <jeffwoolums@mac.com>
2025-06-15 07:47:23 -0500

    Update TriviaPrompt.js

--- Prompts/TriviaPrompt.js
+++ Prompts/TriviaPrompt.js
@@ -2,4 +2,4 @@ function buildTriviaPrompt(request) {
     // Example implementation
     return "Generate a triviaprompt prompt based on this request: " + JSON.stringify(request);
 }
-module.exports = { buildTriviaPrompt };
+module.exports = { buildTriviaPrompt }; 
