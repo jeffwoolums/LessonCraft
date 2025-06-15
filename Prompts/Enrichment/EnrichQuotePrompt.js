6f24df14a23e0226af1fec8c6d07fade7f9196b8
jeffwoolums <jeffwoolums@mac.com>
2025-06-15 07:52:28 -0500

    Update EnrichQuotePrompt.js

--- Prompts/Enrichment/EnrichQuotePrompt.js
+++ Prompts/Enrichment/EnrichQuotePrompt.js
@@ -2,4 +2,4 @@ function buildEnrichQuotePrompt(request) {
     // Provide enrichment-specific prompt logic here.
     return "Enrichment prompt (buildEnrichQuotePrompt) with request: " + JSON.stringify(request);
 }
-module.exports = { buildEnrichQuotePrompt };
+module.exports = { buildEnrichQuotePrompt }; 
