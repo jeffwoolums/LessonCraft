e5921516f0fe30139b99d347f9e8483a7d1905cd
jeffwoolums <jeffwoolums@mac.com>
2025-06-15 07:47:43 -0500

    Update StudyPrompt.js

--- Prompts/StudyPrompt.js
+++ Prompts/StudyPrompt.js
@@ -2,4 +2,4 @@ function buildStudyPrompt(request) {
     // Example implementation
     return "Generate a studyprompt prompt based on this request: " + JSON.stringify(request);
 }
-module.exports = { buildStudyPrompt };
+module.exports = { buildStudyPrompt }; 
